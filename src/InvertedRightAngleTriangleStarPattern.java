12277976f7bb25f2619b5a6715547c78062b8d0b
Abishek Thankaswamy <abishekthankaswamy28@gmail.com>
2023-01-22 08:12:07 +0530

    First Commit

---
+++ src/InvertedRightAngleTriangleStarPattern.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class InvertedRightAngleTriangleStarPattern {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a Number : ");
+
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(" * ");
+            }
+            System.out.println("\n");
+        }
+
+    }
+}
+
+
+/*                  Input = 4;
+
+                     *  *  *  *
+
+                     *  *  *  *
+
+                     *  *  *  *
+
+                     *  *  *  *
+
+
+
+                     Input = 5;
+
+                     *  *  *  *  *
+
+                     *  *  *  *  *
+
+                     *  *  *  *  *
+
+                     *  *  *  *  *
+
+                     *  *  *  *  *
+ */
