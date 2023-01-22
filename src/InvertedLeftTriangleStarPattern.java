f6be7be42f7a3c97688b09b6f0babb7c9f58fc40
Abishek Thankaswamy <abishekthankaswamy28@gmail.com>
2023-01-22 09:21:25 +0530

    First Commit

---
+++ src/InvertedLeftTriangleStarPattern.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class InvertedLeftTriangleStarPattern {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a Number : ");
+
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<i;j++){
+                System.out.print("  ");
+            }
+            for(int l=i;l<=n;l++){
+                System.out.print("* ");
+            }
+
+            System.out.println("\n");
+        }
+
+
+
+    }
+}
+
+
+/*                  Input = 4;
+
+
+                     *  *  *  *
+
+                        *  *  *
+
+                           *  *
+
+                              *
+
+
+
+
+                     Input = 5;
+
+                     *  *  *  *  *
+
+                        *  *  *  *
+
+                           *  *  *
+
+                              *  *
+
+                                 *
+ */
---
+++ src/LeftAngleTriangleStarPattern.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class LeftAngleTriangleStarPattern {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a Number : ");
+
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+
+            System.out.println("\n");
+        }
+
+
+
+    }
+}
+
+
+/*                  Input = 4;
+
+                              *
+
+                           *  *
+
+                        *  *  *
+
+                     *  *  *  *
+
+
+
+                     Input = 5;
+
+                                 *
+
+                              *  *
+
+                           *  *  *
+
+                        *  *  *  *
+
+                     *  *  *  *  *
+ */
