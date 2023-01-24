c59f1ca2cce15a49e9349a76a7bfe724f858aa02
Abishek Thankaswamy <abishekthankaswamy28@gmail.com>
2023-01-24 17:02:26 +0530

    First Commit

---
+++ src/ContinueDemo.java
@@ -0,0 +1,14 @@
+public class ContinueDemo {
+    public static void main(String[] args){
+
+        for(int i=1; i<=100;i++){
+
+            if(i%10==0){
+                continue;
+            }
+            System.out.print(i+", ");
+
+        }
+
+    }
+}
