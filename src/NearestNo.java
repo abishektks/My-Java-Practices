6a5e4223f2324b93bd8b419c7d0a264dcd51319c
Abishek Thankaswamy <abishekthankaswamy28@gmail.com>
2023-01-24 12:10:56 +0530

    First Commit

--- src/NearestNo.java
+++ src/NearestNo.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class NearestNo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a Number : ");
+        int num = scanner.nextInt();
 
+        for(int i=num, j=num; i<=0 | j>=0; i++,j--){
+            if(i%10==0){
+                System.out.println(i);
+                break;
+            } else if (j%10==0) {
+                System.out.println(j);
+                break;
+            }
+
+        }
     }
 }
